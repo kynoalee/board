e49ccc17b1557f05ba6eb08d7cca1a5f4bc43948
고민균 <minkyun93@gmail.com>
2020-04-22 14:43:00 +0900

    입찰 페이지  DB 연결 주문 라우터 내부 상태 변화에 쉽도록 구성 리팩키징 주문없을시 경고창 생성

--- routes/order.js
+++ routes/order.js
@@ -109,7 +109,7 @@ router.get('/list',util.isLoggedin,function(req,res){
     // test 이후 req.user.userid 
     Order.Summary.find({$or:[{orderid : req.user.userid},{vender : req.user.userid}]},function(err,arr){
         if(!arr.length){
-            // 주문없을시 처리해야함.TODO:
+            req.flash("errors",[{message : "주문이 없습니다."}]);
             return res.redirect('/');
         }
         var summary = [];
@@ -128,7 +128,7 @@ router.get('/list',util.isLoggedin,function(req,res){
                 orderH : moment(ob.orderdate).format("HH:mm:ss"),
                 modiD : moment(ob.mdate).format("YYYY-MM-DD"),
                 modiH : moment(ob.mdate).format("HH:mm:ss")
-            }
+            };
 
             let stat = '';
             switch(ob.status){
@@ -187,6 +187,8 @@ router.get('/list',util.isLoggedin,function(req,res){
             return a.ordernum < b.ordernum ? -1 : a.ordernum > b. ordernum ? 1 : 0;
         });
 
+        // Last Order
+
         Order.Last.find({ordernum:initialOrdernum},function(err1,last){
             if(err1){
                 console.log(err1);
@@ -198,7 +200,29 @@ router.get('/list',util.isLoggedin,function(req,res){
                     return res.redirect('/');
                 }
                 var lastOrderNum = initialOrdernum;
-                var lastOrder = [];
+                var lastOrder = [
+                    {},
+                    {
+                        type : "not",
+                        filename : 'ptmaking'
+                    },
+                    {
+                        type : "not",
+                        filename : 'ptdeli'
+                    },
+                    {
+                        type : "not",
+                        filename : 'making'
+                    },
+                    {
+                        type : "not",
+                        filename : 'deli'
+                    },
+                    {
+                        type : "not",
+                        filename : 'done'
+                    }
+                ];
                 if(last[0].summary1){
                     let routerPath =last[0].filepath1.replace("../files","");
                     let fileType = routerPath.split('/');
@@ -219,12 +243,7 @@ router.get('/list',util.isLoggedin,function(req,res){
                         path : routerPath,
                         title : nameSetting.statusName.status2
                     };
-                } else {
-                    lastOrder[1] ={
-                        type : "not",
-                        filename : 'ptmaking'
-                    };
-                }
+                } 
                 if(last[0].summary3){
                     let routerPath =last[0].filepath3.replace("../files","");
                     let fileType = routerPath.split('/');
@@ -234,12 +253,7 @@ router.get('/list',util.isLoggedin,function(req,res){
                         path : routerPath,
                         title : nameSetting.statusName.status3
                     };
-                } else {
-                    lastOrder[2] ={
-                        type : "not",
-                        filename : 'ptdeli'
-                    };
-                }
+                } 
                 if(last[0].summary4){
                     let routerPath ='';
                     let fileType = 'not';
@@ -253,12 +267,7 @@ router.get('/list',util.isLoggedin,function(req,res){
                         path : routerPath,
                         title : nameSetting.statusName.status4
                     };
-                } else {
-                    lastOrder[3] ={
-                        type : "not",
-                        filename : 'making'
-                    };
-                }
+                } 
                 if(last[0].summary5){
                     let routerPath =last[0].filepath5.replace("../files","");
                     let fileType = routerPath.split('/');
@@ -268,12 +277,7 @@ router.get('/list',util.isLoggedin,function(req,res){
                         path : routerPath,
                         title : nameSetting.statusName.status5
                     };
-                } else {
-                    lastOrder[4] ={
-                        type : "not",
-                        filename : 'deli'
-                    };
-                }
+                } 
                 if(last[0].summary6){
                     let routerPath =last[0].filepath6.replace("../files","");
                     let fileType = routerPath.split('/');
@@ -283,12 +287,7 @@ router.get('/list',util.isLoggedin,function(req,res){
                         path : routerPath,
                         title : nameSetting.statusName.status6
                     };
-                }  else {
-                    lastOrder[5] ={
-                        type : "not",
-                        filename : 'done'
-                    }
-                }
+                }  
                 // 디테일 링크 설정
                 var orderDetail = {
                     status1 : [],
@@ -380,14 +379,41 @@ router.get('/list',util.isLoggedin,function(req,res){
     });   
 });
 
-// vender order check bid in
+// vender order check bid in util.isLoggedIn,
 router.get('/bidVenderIn',function(req,res){
-    var order = req.flash('order')[0] || {};
-    var errors = req.flash('errors')[0] || {};
-    res.render('order/bidVenderIn',{
-        order : order,
-        errors : errors
+     
+    Order.Summary.find({vender:{$exists : false},status : 1},function(err,summary){
+        if(err){
+            console.log(err);
+            req.flash("errors",[{message : "DB error"}]);
+            return res.redirect("/");
+        }
+        console.log(summary);
+        let summaryList = [];
+        for(let obj of summary){
+            let dateOb ={
+                orderD : moment(obj.orderdate).format("YYYY-MM-DD"),
+                orderH : moment(obj.orderdate).format("HH:mm:ss"),
+                modiD : moment(obj.mdate).format("YYYY-MM-DD"),
+                modiH : moment(obj.mdate).format("HH:mm:ss")
+            };
+
+            let tmpSummary = {};
+            tmpSummary.ordernum = obj.ordernum;
+            tmpSummary.orderdateD = dateOb.orderD;
+            tmpSummary.orderdateH = dateOb.orderH;
+            tmpSummary.modidateD = dateOb.modiD;
+            tmpSummary.modidateH = dateOb.modiH;
+            tmpSummary.status = obj.status;
+            summaryList[summaryList.length] = tmpSummary;
+        }
+        console.log(summaryList);
+        res.render('order/bidVenderIn',{
+            summary : summaryList
+        });
     });
+   
+    
 });
 
 module.exports = router;
