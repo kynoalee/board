3ee9c0d982d53c5c36d08e5c925c7fdd542ecde7
고민균 <minkyun93@gmail.com>
2020-04-24 19:24:36 +0900

    온클릭 이벤트 작성중

--- public/js/order.js
+++ public/js/order.js
@@ -8,13 +8,12 @@ function goLastOrder(num){
 function goPopup(ordernum,status){
     window.open("/pop/detail?ordernum="+ordernum+"&status="+status,"popId"+status,'width=430,height=500,location=no,status=no,scrollbars=yes');
 }
-$(document).ready(function(){
+function test(test){
+    if($(this).attr('class') == 'test'){
+        $(this).attr('class','');
+    } else{
+        $(this).attr('class','test');
+    }
+    alert($(this).attr('class'));
 
-    $(".test").on("change",function(){
-        alert($(this).attr('class'));
-        $(this).addClass('done');
-        alert($(this).attr('class'));
-        $(this).removeClass('test');
-        alert($(this).attr('class'));
-    });
-});
+}
