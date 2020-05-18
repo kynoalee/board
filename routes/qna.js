c07cde9e8594bb66e12d2a38cb8df3480a1527cf
고민균 <minkyun93@gmail.com>
2020-05-18 16:59:13 +0900

    페이징 완성.

--- routes/qna.js
+++ routes/qna.js
@@ -1,12 +1,14 @@
 var express  = require('express');
 var router = express.Router();
 var Board = require('../models/Board');
+var Log = require('../models/Log');
 
 router.get('/',(req,res)=>{
-    var userid = 'imgcom';//req.user.userid;
-    var pageNum = req.query.pageNum || 2;
-    var boardNum = req.query.boardNum || 7;
-    var findObj = {
+    const userid = 'imgcom';//req.user.userid;
+    const pageNum = req.query.pageNum || 1; // 페이지 포인터
+    const boardNum = req.query.boardNum || 10; // 게시물 갯수 
+    const blockNum = 5; // 페이징 보여줄 갯수. 
+    const findObj = {
         userid : userid,
         parents : -1
     };
@@ -27,8 +29,50 @@ router.get('/',(req,res)=>{
                 return res.redirect('/');
             }
             var boardLists = [];
-            var idNum = (pageNum-1)*boardNum+1;
+            var last = Math.ceil(c/boardNum); // 완전 마지막 페이지넘버
+            var nowBlock = pageNum%blockNum==0 ? parseInt(pageNum/blockNum):parseInt(pageNum/blockNum)+1;
+            var EndBlock = last%blockNum==0 ? parseInt(last/blockNum) : parseInt(last/blockNum)+1;
+            var goPre = 'display-none';
+            var goNext = 'display-none';
+            var preNum = 0; // 전 블록 가기 버튼 포인터
+            var nextNum = 0; // 다음 블록 가기 버튼 포인터
+            
+            var start = (nowBlock-1)*blockNum + 1; // 현재 페이지의 첫 페이지넘버
+            var end = start + blockNum - 1; // 현재 페이지의 마지막 페이지넘버
+            end = end > last ? last : end;
+
+            // 넘어온 pageNum 유효성 체크
+            if(pageNum <= 0 || pageNum > last){
+                Log.create({document_name : "Board",type:"error",contents:{pageNum:pageNum,content:"문의 리스트 페이지넘버 유효성 에러"},wdate:Date()});
+                console.log("pageNum : " + pageNum + ", 페이지넘버 유효하지 않음.");
+                req.flash("errors",{message : "Page ERROR"});
+                return res.redirect('/');
+            }
+
+            // block 이동 버튼 디스플레이 여부
+            if(nowBlock != 1){
+                goPre = '';
+                preNum = start - 1;
+            }
+            if(nowBlock!=EndBlock){
+                goNext = '';
+                nextNum = start + blockNum;
+            }
+
+            // 페이징 관련 객체
+            var pages = {
+                start : start,
+                end : end,
+                goPre : goPre,
+                pre : preNum,
+                goNext : goNext,
+                next : nextNum,
+                last : last,
+                nowPage : pageNum
+            };
+            var idNum = (pageNum-1)*boardNum+1; // 게시물 자체 번호 전체 갯수에 의존.
             for(let val of board){
+
                 let tmpObj = {
                     idNum : idNum,
                     test : val.qnanum
@@ -37,7 +81,8 @@ router.get('/',(req,res)=>{
                 idNum++;
             }
             res.render('qna/qnaList',{
-                boardList : boardLists
+                boardList : boardLists,
+                pages : pages
             });
         });
     });
