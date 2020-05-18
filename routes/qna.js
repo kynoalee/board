dbaaef0d6cf6c2d19a809492e8a704fd8d874d76
고민균 <minkyun93@gmail.com>
2020-05-18 14:48:33 +0900

    게시판의 기본 디비 연결과 페이징 제작. 문의 게시판에 올라올 문의는 최상위 문의만 올릴 예정임. 연결 문의들은 해당 문의 링크를 따라가면 있도록 함. 문의 쓰기는 없으며, 읽기 전용이고, 링크는 다른곳에서 연결되는 곳과 같은 곳으로 연결.

--- routes/qna.js
+++ routes/qna.js
@@ -1,8 +1,46 @@
 var express  = require('express');
 var router = express.Router();
+var Board = require('../models/Board');
 
 router.get('/',(req,res)=>{
-    res.render('/qna');
+    var userid = 'imgcom';//req.user.userid;
+    var pageNum = req.query.pageNum || 2;
+    var boardNum = req.query.boardNum || 7;
+    var findObj = {
+        userid : userid,
+        parents : -1
+    };
+    Board.find({}).sort({qnanum:1}).skip((pageNum-1)*boardNum).limit(boardNum).exec((err,board)=>{
+        if(err){
+            Log.create({document_name : "Board",type:"error",contents:{error:err,content:"문의 리스트 생성을 위한 문의 내용 find 중 DB 에러"},wdate:Date()});
+            console.log(err);
+            req.flash("errors",{message : "DB ERROR"});
+            return res.redirect('/');
+        }
+
+        // 전체 갯수 파악
+        Board.countDocuments({},(err2,c)=>{
+            if(err2){
+                Log.create({document_name : "Board",type:"error",contents:{error:err2,content:"문의 리스트 전체 갯수 count 중 DB 에러"},wdate:Date()});
+                console.log(err2);
+                req.flash("errors",{message : "DB ERROR"});
+                return res.redirect('/');
+            }
+            var boardLists = [];
+            var idNum = (pageNum-1)*boardNum+1;
+            for(let val of board){
+                let tmpObj = {
+                    idNum : idNum,
+                    test : val.qnanum
+                };
+                boardLists.push(tmpObj);
+                idNum++;
+            }
+            res.render('qna/qnaList',{
+                boardList : boardLists
+            });
+        });
+    });
 });
 
 module.exports = router;
