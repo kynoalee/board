f38a51e3223d2d6aa8d1dce87ca781e1d2d7564f
고민균 <minkyun93@gmail.com>
2020-07-24 18:40:14 +0900

    유저 내용 조회 및 수정 할 수 있게 만듬 테이블 세팅과 팝업 세팅은 다른 곳에서 그대로 사용하면 됨

--- routes/pd/user.js
+++ routes/pd/user.js
@@ -3,6 +3,7 @@ var express  = require('express');
 var router = express.Router();
 var util = require('./pdUtil');
 var User = require('../../models/User');
+var Log = require('../../models/Log');
 
 router.get('/',util.isLogIn,function(req,res){
      return res.redirect('/pd/user/customer');
@@ -32,8 +33,20 @@ router.get('/customer',util.isLogIn,function(req,res){
     });
 });
 
-router.get('/customer/detail',function(req,res){
-    return res.render('pd/user/customerDetail');
+router.get('/customer/detail',util.isLogIn,function(req,res){
+    User.findOne({userid:req.query.userid,userclass:"normal"},(err,usr)=>{
+        if(err){
+            Log.create({document_name : "User",type:"error",contents:{error:err,content:"pd customer 상세 조회 DB에러"},wdate:Date()});
+            console.log(err);
+            req.flash("errors",{message : "DB ERROR"});
+            return res.redirect('/pop/close');
+        }
+        usr.wdateFormated = moment(usr.wdate).format("YYYY-MM-DD HH:mm:ss");
+        usr.mdateFormated = moment(usr.mdate).format("YYYY-MM-DD HH:mm:ss");
+        return res.render('pd/user/customerDetail',{
+            userData : usr
+        });
+    });
 });
 
 router.get('/vender',util.isLogIn,function(req,res){
@@ -41,9 +54,39 @@ router.get('/vender',util.isLogIn,function(req,res){
     var menu2= new util.menu("벤더 정보 조회/수정","","selected");
     var menu3= new util.menu("PD 정보 조회/수정","/pd/user/admin","");
 
-     return res.render('pd/user/vender',{
-         menu : [menu1,menu2,menu3]
-     });
+    User.find({userclass:'vender'},(err,venders)=>{
+        if(err){
+            Log.create({document_name : "User",type:"error",contents:{error:err,content:"pd vender find DB에러"},wdate:Date()});
+            console.log(err);
+            req.flash("errors",{message : "DB ERROR"});
+            return res.redirect('/pd');
+        }
+        for(var vender of venders){
+            vender.wdateFormated = moment(vender.wdate).format("YYYY-MM-DD HH:mm:ss");
+            vender.mdateFormated = moment(vender.mdate).format("YYYY-MM-DD HH:mm:ss");
+
+        }
+        return res.render('pd/user/vender',{
+            venders : venders,
+            menu : [menu1,menu2,menu3]
+        });
+    });
+});
+
+router.get('/vender/detail',util.isLogIn,function(req,res){
+    User.findOne({userid:req.query.userid,userclass:"vender"},(err,vender)=>{
+        if(err){
+            Log.create({document_name : "User",type:"error",contents:{error:err,content:"pd vender 상세 조회 DB에러"},wdate:Date()});
+            console.log(err);
+            req.flash("errors",{message : "DB ERROR"});
+            return res.redirect('/pop/close');
+        }
+        vender.wdateFormated = moment(vender.wdate).format("YYYY-MM-DD HH:mm:ss");
+        vender.mdateFormated = moment(vender.mdate).format("YYYY-MM-DD HH:mm:ss");
+        return res.render('pd/user/venderDetail',{
+            userData : vender
+        });
+    });
 });
 
 router.get('/admin',util.isLogIn,function(req,res){
